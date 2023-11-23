34a546464706e9f03d0ffb8f69808d2c5086080d
Sak <jacubsak@gmail.com>
2023-11-23 21:09:07 +0100

    wypisuje 30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mini-facebook';
 }
+
+// export const routes: Routes = [
+//   {path: 'users', component: UserComponent},
+  
+// ];
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,22 +3,30 @@ import { CommonModule } from '@angular/common';
 
 
 import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  providers:[UserService],
 })
-export class UserComponent  {
+export class UserComponent  implements OnInit{
+
+  users:User[]=[];
+
+
+
+  constructor(private userService:UserService){}
 
 
 
 
-
-
-
-  constructor(){}
-
+  ngOnInit(): void {
+      console.log("User component OnInit");
+      this.users = this.userService.getUsers();
+  }
 }
+
