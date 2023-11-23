bc0051e659191baf5cc0229420661f7578222a92
muzykos <77461733+muzykos@users.noreply.github.com>
2023-11-23 22:05:39 +0100

    user spec ts

--- src/app/models/user.model.spec.ts
+++ src/app/models/user.model.spec.ts
@@ -2,6 +2,6 @@ import { User } from './user.model';
 
 describe('User', () => {
   it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
+    expect(new User("user","name","surname","pwd","email",696969696)).toBeTruthy();
   });
 });
